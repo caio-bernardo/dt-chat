3962fd1528bef441ea3d7c181f98a74660142e49
Caio Bernardo <me@caiobernardo.top>
2026-03-09 17:51:23 -0300

    refactor(fork_engine): retreat some changes

--- apps/fork_engine/src/fork_engine/helpers.py
+++ apps/fork_engine/src/fork_engine/helpers.py
@@ -1,31 +1,18 @@
-import os
 from typing import Sequence
+
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, AnyMessage, HumanMessage
 
 from bancobot.database import MessageType
 from bancobot.models import Message
-from dotenv import load_dotenv
-from langchain_core.messages import AIMessage, AnyMessage, HumanMessage
-from pydantic import UUID4
-from sqlmodel import Session, col, create_engine, select
 
 load_dotenv()
 
-DB_URL = os.environ["DB_URL"]
-
-engine = create_engine(DB_URL)
-storage = Session(engine)
+BANCOBOT_URL = "http://localhost:8000"
 
 
-def load_history(session_id: UUID4, limit: int) -> Sequence[Message]:
-    try:
-        return storage.exec(
-            select(Message)
-            .order_by(col(Message.created_at))
-            .where(Message.session_id == id)
-            .limit(limit)
-        ).all()
-    except Exception as e:
-        raise e
+def load_history(session_id: int, limit: int) -> Sequence[Message]:
+    return []
 
 
 def map_internal_2_langchain_message(msg: Message) -> AnyMessage:
